a896d0c7f92c783dff2c56d65ea11b167ee3e736
KeyJoo <vladimir.pavlychev@osarts.ru>
2018-01-01 09:52:27 +0300

    Readme - Added 1st script; Added var = guess

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # HeadFirstRuby
 Learning Ruby-lang from a book "HeadFirstRuby"
+
+
+## 1st Script "Get My Number "
+
+You need get true number between 0 to 100 no more 7 guesses.
+_2018.01.01  09:50_
--- get_number.rb
+++ get_number.rb
@@ -2,7 +2,32 @@
 # Written by : Vladimir Pavlychev
 
 puts "Welcome to 'Get My Number!"
-print "What's your name? "
 
+# Getting name of player and print welcome.
+print "What's your name? "
 input = gets
- puts "Welcome, #{input}"
+name = input.chomp
+puts "Welcome, in \"Ruby Head First\" #{name}!"
+
+# Saving random number.
+puts "I've got a random number betwenn 1 and 100"
+puts "Can you guess it?"
+target = rand(101)
+
+# Seeking for guesses
+num_guesses = 0
+
+#remaining_guesses = 10 - num_guesses
+#puts remaining_guesses.to_s + " guesses left."
+puts "You've got #{10 - num_guesses} guesses left"
+
+
+puts "Make a guess: "
+guess = gets.to_i
+
+puts guess < target 
+puts guess > target
+
+#sleep 2
+#puts target
+
