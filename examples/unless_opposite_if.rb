452008ac43ca9a857574fb1c17820344338c3da3
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2018-01-03 18:55:27 +0300

    add examples/unless_opposite_if.rb

---
+++ examples/unless_opposite_if.rb
@@ -0,0 +1,12 @@
+# unless_opposite_if.rb
+# coding: utf-8
+# "unless" как противоположность "if"
+
+if ! = (light == "red")
+	puts "Go!"
+end
+
+# new syntax:
+unless light == "red"
+	puts "Go!"
+end
