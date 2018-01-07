36ae9d5f2783119b1b2341770c72435459517362
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2018-01-07 23:23:39 +0300

    2 arguments

--- examples/methods_and_arguments.rb
+++ examples/methods_and_arguments.rb
@@ -1,10 +1,10 @@
 # /examples/methods_and_arguments.rb
 
-def hello_world
+def hello_world(first_name,last_name)
 	puts "Hello world!" 
-	puts "My name is Alexander Lesyunin"
+	puts "My name is " + first_name + " " + last_name 
 end
 
-hello_world
-hello_world
-hello_world
+hello_world("Vladimir", "Lenin")
+hello_world("John", "Lennon")
+hello_world("Alexander", "Lesyunin")
