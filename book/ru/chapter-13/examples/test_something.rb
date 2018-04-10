c4ef08d09358d65165babf891eb0d89ebceffbed
Vladimir Pavlychev <vladimir.pavlychev@OsArts.ru>
2018-04-10 07:45:17 +0300

    Add minitest template(clear)

---
+++ book/ru/chapter-13/examples/test_something.rb
@@ -0,0 +1,15 @@
+# test_something.rb
+# coding: utf-8
+# 2018-04-10 07:40
+# [HFR_p.425]
+
+require 'minitest/autorun'
+
+class TestSomething < Minitest::Test
+	def test_true_assertion
+		assert(true)
+	end
+
+	def test_false_assertion
+		assert(false)
+	end
