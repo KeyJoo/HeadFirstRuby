cb02f6d2b5685be1c784e142ec424d37dd64a825
Vladimir Pavlychev <vladimir.pavlychev@osarts.ru>
2018-09-03 23:57:55 +0300

    :bug: fix sintaxis

--- code solutions/get_number.rb
+++ code solutions/get_number.rb
@@ -2,77 +2,75 @@
 # Written by : Vladimir Pavlychev https://github.com/KeyJoo
 # coding: utf-8
 
-def colorize(text, color = "default", bgColor = "default")
-    
-    colors = {
-      "default" => "38",
-      "black" => "30",
-      "red" => "31",
-      "green" => "32",
-      "brown" => "33", 
-      "blue" => "34", 
-      "purple" => "35",
-      "cyan" => "36", 
-      "gray" => "37", 
-      "dark gray" => "1;30", 
-      "light red" => "1;31", 
-      "light green" => "1;32", 
-      "yellow" => "1;33",
-      "light blue" => "1;34", 
-      "light purple" => "1;35", 
-      "light cyan" => "1;36", 
-      "white" => "1;37"}
-    
-    bgColors = {
-      "default" => "0", 
-      "black" => "40", 
-      "red" => "41", 
-      "green" => "42", 
-      "brown" => "43", 
-      "blue" => "44",
-      "purple" => "45", 
-      "cyan" => "46", 
-      "gray" => "47", 
-      "dark gray" => "100", 
-      "light red" => "101", 
-      "light green" => "102",
-      "yellow" => "103", 
-      "light blue" => "104", 
-      "light purple" => "105", 
-      "light cyan" => "106", 
-      "white" => "107"}
+def colorize(text = '_', color = 'default', bgcolor = 'default')
+  colors = {
+    'default' => 38,
+    'black' => 30,
+    'red' => 31,
+    'green' => 32,
+    'brown' => 33,
+    'blue' => 34,
+    'purple' => 35,
+    'cyan' => 36,
+    'gray' => 37,
+    'dark gray' => '1;30',
+    'light red' => '1;31',
+    'light green' => '1;32',
+    'yellow' => '1;33',
+    'light blue' => '1;34',
+    'light purple' => '1;35',
+    'light cyan' => '1;36',
+    'white' => '1;37'
+  }
+  bgcolors = {
+    'default' => 0,
+    'black' => 40,
+    'red' => 41,
+    'green' => 42,
+    'brown' => 43,
+    'blue' => 44,
+    'purple' => 45,
+    'cyan' => 46,
+    'gray' => 47,
+    'dark gray' => 100,
+    'light red' => 101,
+    'light green' => 102,
+    'yellow' => 103,
+    'light blue' => 104,
+    'light purple' => 105,
+    'light cyan' => 106,
+    'white' => 107
+  }
 
-    color_code = colors[color]
-    bgColor_code = bgColors[bgColor]
+  color_code = colors[color]
+  bgcolor_code = bgcolors[bgcolor]
 
-    return "\033[#{bgColor_code};#{color_code}m#{text}\033[0m"
+  return "\033[#{bgcolor_code};#{color_code}m#{text}\033[0m"
 end
 
-
-puts "Welcome to #{colorize("Get My Number!", "white","light red")}"
+puts "Welcome to #{colorize('Get My Number!', 'white', 'light red')}"
 
 # Getting name of player and print welcome.
 print "What's your name? "
 input = gets
 name = input.chomp
-puts "Welcome, in \"Ruby Head First\" #{colorize(name, "white","light blue")}!"
+puts "Welcome, in \'Ruby Head First\' #{colorize(name, 'white', 'light blue')}!"
 
 # Saving random number.
-puts "I've got a random number betwenn 1 and 100"
-puts "Can you guess it?"
+puts "I've got a random number between 1 and 100"
+puts 'Can you guess it?'
 target = rand(101)
 
 # Track how many guesses the player has made.
 num_guesses = 0
 
-
-#Track wether the player has guessed correctly.
+# Track wether the player has guessed correctly.
 guessed_it = false
 
 until num_guesses == 7 || guessed_it
 
   puts "You've got #{7 - num_guesses} guesses left."
-  print "Make a guess: "
+  print 'Make a guess: '
   guess = gets.to_i
 
   num_guesses += 1
@@ -80,13 +78,11 @@ until num_guesses == 7 || guessed_it
   # Compare the guess to the target.
   # Print the appropriate message.
   if guess < target
-    puts "Oops. #{colorize("Your guess was LOW.","white", "green" )}"
-  
+    puts "Oops. #{colorize('Your guess was LOW.', 'white', 'green')}"
   elsif guess > target
-    puts "Oops. #{colorize("Your guess was HIGH.","white", "red" )}"
-  
+    puts "Oops. #{colorize('Your guess was HIGH.', 'white', 'red')}"
   elsif guess == target
-    puts "Good job, #{colorize(name, "white","light blue")}!"
+    puts "Good job, #{colorize(name, 'white', 'light blue')}!"
     puts "You guessed my number (#{target}) in #{num_guesses} guesses!"
     guessed_it = true
   end
